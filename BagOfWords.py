f49c1f09ebfcfeb04e54219bf02c416462d9a6b1
Leonardo Ikeda <80907803+ikedss@users.noreply.github.com>
2022-10-20 19:43:01 -0300

    Create BagOfWords.py

---
+++ BagOfWords.py
@@ -0,0 +1,38 @@
+# Aluno: Leonardo Ikeda
+
+"""
+Sua tarefa será  gerar a matriz termo documento, dos documentos recuperados da internet e 
+imprimir esta matriz na tela. Para tanto: 
+a) Considere que todas as listas de sentenças devem ser transformadas em listas de vetores, 
+onde cada item será uma das palavras da sentença. 
+b) Todos  os  vetores  devem  ser  unidos  em  um  corpus  único  formando  uma  lista  de  vetores, 
+onde cada item será um lexema.  
+c) Este único corpus será usado para gerar o vocabulário. 
+d) O  resultado  esperado  será  uma  matriz  termo  documento  criada  a  partir  da  aplicação  da 
+técnica bag of Words em todo o corpus.  
+"""
+
+from bs4 import BeautifulSoup
+import requests
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+
+def adiciona_site(site, lsentencas):
+    html = requests.get(site)
+    soap = BeautifulSoup(html.content, 'html.parser')
+
+    for doc in soap.find_all('p'):
+        documento = nlp(doc.text)
+        for sentenca in documento:
+            lsentencas.append(sentenca)
+    return lsentencas
+
+lsentencas = []
+sentencas1 = adiciona_site("https://en.wikipedia.org/wiki/Natural_language_processing", lsentencas)
+sentencas2 = adiciona_site("https://www.ibm.com/cloud/learn/natural-language-processing", lsentencas)
+sentencas3 = adiciona_site("https://www.sas.com/en_us/insights/analytics/what-is-natural-language-processing-nlp.html", lsentencas)
+sentencas4 = adiciona_site("https://builtin.com/data-science/high-level-guide-natural-language-processing-techniques", lsentencas)
+sentencas5 = adiciona_site("https://deepsense.ai/a-business-guide-to-natural-language-processing-nlp/", lsentencas)
+
+print(lsentencas)
